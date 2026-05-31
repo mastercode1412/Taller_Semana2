355b22d559fa02b5f21c4ec9783b5a59ddaaa063
Francisco Portocarrero Rios <n00416309@upn.pe>
2026-05-31 13:04:35 -0500

    docs: se agrega encabezado personalizado para control de excepciones en Laptop1

--- src/caso_colaborador/SistemaEstudiantes.java
+++ src/caso_colaborador/SistemaEstudiantes.java
@@ -58,7 +58,7 @@ public class SistemaEstudiantes {
     }
 
     // Método para imprimir el reporte masivo de la colección
- // >>> REPORTE DE CONTROL DE EXCEPCIONES: HISTORIAL DE ESTUDIANTES <<<.
+ // >>> REPORTE DE CONTROL DE EXCEPCIONES: HISTORIAL DE ESTUDIANTES <<<..
     public void mostrarListaCompleta() {
         System.out.println("=========================================");
         System.out.println("     REPORTE FINAL: LISTA DE ESTUDIANTES ");
