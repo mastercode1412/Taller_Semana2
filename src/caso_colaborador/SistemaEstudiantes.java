996b00463f0b9dec5d2cfd9c8514ba1f349ab011
francport <francport@gmail.com>
2026-05-31 13:00:56 -0500

    docs: se agrega cambio en la estética del reporte.

--- src/caso_colaborador/SistemaEstudiantes.java
+++ src/caso_colaborador/SistemaEstudiantes.java
@@ -58,6 +58,7 @@ public class SistemaEstudiantes {
     }
 
     // Método para imprimir el reporte masivo de la colección
+    // REPORTE FINAL DE MATRICULA: LISTA OFICIAL DE ESTUDIANTES UPN
     public void mostrarListaCompleta() {
         System.out.println("=========================================");
         System.out.println("     REPORTE FINAL: LISTA DE ESTUDIANTES ");
